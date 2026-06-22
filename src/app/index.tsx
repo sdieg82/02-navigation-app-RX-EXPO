0578c18c96d6e00acd9960f8e60d2748747e6a7e
Diego Sanchez <sedifab@gmail.com>
2026-06-22 18:01:20 -0500

    Routing

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,9 @@
-import * as Device from 'expo-device';
-import { Platform, StyleSheet } from 'react-native';
-import { SafeAreaView } from 'react-native-safe-area-context';
-
-import { AnimatedIcon } from '@/components/animated-icon';
-import { HintRow } from '@/components/hint-row';
+import HomeContent from '@/components/Home.component';
 import { ThemedText } from '@/components/themed-text';
 import { ThemedView } from '@/components/themed-view';
-import { WebBadge } from '@/components/web-badge';
 import { BottomTabInset, MaxContentWidth, Spacing } from '@/constants/theme';
+import * as Device from 'expo-device';
+import { Platform, StyleSheet } from 'react-native';
 
 function getDevMenuHint() {
   if (Platform.OS === 'web') {
@@ -31,32 +27,7 @@ function getDevMenuHint() {
 export default function HomeScreen() {
   return (
     <ThemedView style={styles.container}>
-      <SafeAreaView style={styles.safeArea}>
-        <ThemedView style={styles.heroSection}>
-          <AnimatedIcon />
-          <ThemedText type="title" style={styles.title}>
-            Welcome to&nbsp;Expo
-          </ThemedText>
-        </ThemedView>
-
-        <ThemedText type="code" style={styles.code}>
-          get started
-        </ThemedText>
-
-        <ThemedView type="backgroundElement" style={styles.stepContainer}>
-          <HintRow
-            title="Try editing"
-            hint={<ThemedText type="code">src/app/index.tsx</ThemedText>}
-          />
-          <HintRow title="Dev tools" hint={getDevMenuHint()} />
-          <HintRow
-            title="Fresh start"
-            hint={<ThemedText type="code">npm run reset-project</ThemedText>}
-          />
-        </ThemedView>
-
-        {Platform.OS === 'web' && <WebBadge />}
-      </SafeAreaView>
+      <HomeContent></HomeContent>
     </ThemedView>
   );
 }
---
+++ src/components/Home.component.tsx
@@ -0,0 +1,12 @@
+import { Text } from 'react-native'
+import { ThemedView } from './themed-view'
+
+const HomeContent = () => {
+  return (
+    <ThemedView>
+      <Text>HomeScreen</Text>
+    </ThemedView>
+  )
+}
+
+export default HomeContent
